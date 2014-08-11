850e82e2b3636abf866f3e718171613151136f74
Brecht Hoflack <brecht.hoflack@gmail.com>
2014-08-11 21:10:41 +0200

    Added a part of the flow

--- tests/Runner.hs
+++ tests/Runner.hs
@@ -1,21 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Lens ((&), (.~))
+import Data.ByteString (ByteString)
+
 import Network.HTTP.Types.Method (methodGet)
-import Network.HTTP.Types.Status (status200, status404)
+import Network.HTTP.Types.Status (Status)
 
 import Restmachine.Core (run)
-import Restmachine.Core.Types (Request (..), Response (..))
+import Restmachine.Core.Types
 
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.HUnit (Assertion, assertEqual, (~=?))
 
+import qualified Network.HTTP.Types.Status as H
+
 defaultRequest = Request methodGet [] "hello world"
 
-noResources :: Assertion
-noResources = do
-  resp <- run defaultRequest
-  assertEqual "" (Response status404 [] "") resp
+verifyResponse :: Resource -> Status -> Assertion
+verifyResponse res stat = do
+  resp <- run res defaultRequest
+  assertEqual "" (Response stat [] "") resp
+
+unavailable    = verifyResponse defaultResource H.status503
+notImplemented = verifyResponse (defaultResource & serviceAvailable  .~ \_ -> return True) H.status501
+longUri        = verifyResponse (defaultResource & (serviceAvailable .~ \_ -> return True)
+                                                 & (knownMethod      .~ \_ -> return True)
+                                                 & (uriTooLong       .~ \_ -> return True)) H.status414
+methodNotAllowed = verifyResponse (defaultResource & (serviceAvailable .~ \_ -> return True)
+                                                   & (knownMethod      .~ \_ -> return True)
+                                                   & (methodAllowed    .~ \_ -> return False)) H.status405
 
 main :: IO ()
 main = defaultMain tests
@@ -23,6 +37,9 @@ main = defaultMain tests
 tests :: [Test]
 tests = [
   testGroup "run" [
-      testCase "no resources" noResources
+      testCase "unavailable" unavailable
+    , testCase "service not implemented" notImplemented
+    , testCase "uri too long" longUri
+    , testCase "method not allowed" methodNotAllowed
     ]
   ]
